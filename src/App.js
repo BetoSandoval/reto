77231d490d647431ad051265884df5df503f2816
BetoSandoval <alberto.sa1391@gmail.com>
2021-09-30 16:40:24 -0500

    fisrt commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import DragDrop from "./components/DragDrop";
 
-function App() {
+
+const App = () => {
+
+  const [file, setFile] = useState();
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type="file"/>
     </div>
   );
 }
 
 export default App;
+
