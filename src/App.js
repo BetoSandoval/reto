85fd83a4b33921b262abd1837a36c4c8ee7cc376
BetoSandoval <alberto.sa1391@gmail.com>
2021-09-30 21:36:28 -0500

    Axios con las constantes FormData y fs

--- src/App.js
+++ src/App.js
@@ -4,11 +4,19 @@ import DragDrop from "./components/DragDrop";
 
 const App = () => {
 
-  const [file, setFile] = useState();
+  const [file, setFile] = useState(null);
+
+  const handleChange = (e) => {
+    const pictureData = (e.target).files[0];
+    setFile(pictureData);
+  }
+
+  console.log(file);
 
   return (
     <div className="App">
-      <input type="file"/>
+      <input type="file" onChange={handleChange}/>
+      <input text='text'/>
     </div>
   );
 }
---
+++ src/action/Action.js
@@ -0,0 +1,25 @@
+import axios from "axios";
+
+
+const FormData = require("form-data");
+const fs = require("fs");
+
+export const upImage = async (file) => {
+  const data = new FormData();
+  data.append(
+    "file",
+    fs.createReadStream(file)
+  );
+
+  const res = await axios({
+    method: "POST",
+    url: `https://whois.nomada.cloud/upload`,
+    data: data,
+    headers: {
+      "Content-Type": "application/json",
+      nomada: "ZWEzMjU1ZmItMDM2MS00YmI4LWJkNzQtMzEwNTk5OGFkZTc1",
+    },
+  });
+
+  return res;
+};
