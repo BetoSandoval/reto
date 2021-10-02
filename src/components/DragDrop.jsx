ef8ad7361733252d242dd8c3cc5a6d2faad7d89d
BetoSandoval <alberto.sa1391@gmail.com>
2021-10-02 02:06:47 -0500

    Request get actorData

--- src/components/DragDrop.jsx
+++ src/components/DragDrop.jsx
@@ -2,44 +2,63 @@ import { useState } from "react";
 
 import axios from "axios";
 
-
-export const upImage = async (file) => {
-  const data = new FormData();
-  data.append("file", file);
-
-  const res = await axios({
-    method: "POST",
-    url: `https://whois.nomada.cloud/upload`,
-    data: data,
-    headers: {
-      "Content-Type": "application/json",
-      nomada: "ZWEzMjU1ZmItMDM2MS00YmI4LWJkNzQtMzEwNTk5OGFkZTc1",
-    },
-  });
-
-  console.log(res.data.actorName);
-  return res.data.actorName;
-
-};
-
-
 export default function DragDrop() {
+
   const [file, setFile] = useState(null);
+  const [actorData, setActorData] = useState(null);
+
+  // Consumo de APIs
+  const upImage = async (file) => {
+    const data = new FormData();
+    data.append("file", file);
+
+    // Eniva foto y recibe nombre
+    const res = await axios({
+      method: "POST",
+      url: `https://whois.nomada.cloud/upload`,
+      data: data,
+      headers: {
+        "Content-Type": "application/json",
+        nomada: "ZWEzMjU1ZmItMDM2MS00YmI4LWJkNzQtMzEwNTk5OGFkZTc1",
+      },
+    });
+
+    const name = res.data.actorName;
+
+    return getDataActor(name);
+  };
+
+  const getDataActor = async( name ) => {
+    const response = await axios({
+      method: "GET",
+      url: `https://api.themoviedb.org/3/search/person?api_key=30db1237b9167f8afaf9e065b90d16b8&language=en-US&query=${name}&page=1&include_adult=false`
+    });
+
+    return setActorData( response);
+  }
 
 
-  //sube la foto  
+  //sube la foto
   const handleChange = (e) => {
     const pictureData = e.target.files[0];
     setFile(pictureData);
   };
 
-  console.log(file);
-
   return (
-    <div>
-      <input type="file" onChange={handleChange} />
-      <button onClick={() => upImage(file) }>Search</button>
 
-    </div>
+    <>
+      { actorData === null ?
+        <div>
+          <input type="file" onChange={handleChange} />
+          <button onClick={() => upImage(file)}>Search</button>
+        </div>
+      :
+         <h1>{JSON.stringify(actorData)}</h1>
+      }
+    </>
+
+    
+
+
   );
 }
